657b5c96b0ff6618e53cb27267dccc4f8529ab9a
muhagungsolihin <muhagungsolihin@gmail.com>
2024-02-07 17:20:28 +0700

    add lingkaran_agung

---
+++ bangundatar/room_1/lingkaran_agung.js
@@ -0,0 +1,13 @@
+function tentukanLuasKelilingSegitigaSamaSisi(alas, tinggi) {
+
+    const luas = (alas * tinggi)/2
+    const keliling = 3 * alas
+
+    //Result//
+    console.log(`Alas : ${alas} cm`);
+    console.log(`Tinggi : ${tinggi} cm`);
+    console.log(`Luas : ${luas} cm`);
+    console.log(`Keliling Segitiga : ${keliling} cm`);
+}
+
+tentukanLuasKelilingSegitigaSamaSisi(12, 30);
