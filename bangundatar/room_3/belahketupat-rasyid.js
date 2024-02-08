04eed81d9eb3ab8d6abee5b75338f0b1dfc33ca0
rasyidtanjung <rasyidtanjung21@gmail.com>
2024-02-08 15:25:46 +0700

    penambahan feature baru

---
+++ bangundatar/room_3/belahketupat-rasyid.js
@@ -0,0 +1,36 @@
+// Jangan sentuh code disini
+function TentukanLuasDanKelilingBelahKetupat(s, d1, d2) {
+ 
+    // TODO: buat logic dibawah sini dimana sisinya adalah 5, diagonal 1-nya adalah 8, dan diagonal 2-nya adalah 7
+  
+    var luas = 0.5 * d1 *d2;
+    var kel = 4 *s;
+  
+    console.log("Sisi belah ketupat:", s, "cm");
+    console.log("Diagonal 1 belah ketupat:", d1, "cm");
+    console.log("Diagonal 2 belah ketupat:", d2, "cm")
+  
+    console.log("Jadi luas belah ketupat : ",luas, "dan kelilingnya : ",kel);
+    // TODO: buatlah logic untuk menghitung luas dan keliling dibawah
+    // TODO: gunakan console.log untuk mencetak hasil luas dan keliling
+    
+    // Akhir pembuatan code
+  
+  }
+  
+  TentukanLuasDanKelilingBelahKetupat(5, 8, 7);
+  
+  /*
+  function luasKelilingLingkaran(r){
+  
+      const pi = 3.14;
+      
+      let luas = pi*r**2;
+    let kel = 2*pi*r;
+      
+      console.log("Jari-jari lingkaran:", r, "cm");
+    console.log("Jadi luas lingkaran : ",luas, "dan kelilingnya : ",kel);
+  }
+  
+  luasKelilingLingkaran(10);
+  */
